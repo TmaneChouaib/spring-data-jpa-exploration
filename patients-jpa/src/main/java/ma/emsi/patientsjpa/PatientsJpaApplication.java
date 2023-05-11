664760e1680d33e90e0f6052aa5c8c844598dbcc
TmaneChouaib <chouaib.tmane@hotmail.com>
2023-05-11 22:50:30 +0100

    first commit

---
+++ patients-jpa/src/main/java/ma/emsi/patientsjpa/PatientsJpaApplication.java
@@ -0,0 +1,13 @@
+package ma.emsi.patientsjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientsJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientsJpaApplication.class, args);
+    }
+
+}
---
+++ patients-jpa/src/test/java/ma/emsi/patientsjpa/PatientsJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.emsi.patientsjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientsJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
